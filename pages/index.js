cf9a6e8f33f6d5dee7e523a14efe8c348a76601a
Gautam Gunecha <gautamgunecha@gmail.com>
2022-09-12 22:58:35 +0530

    lazy-loading

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,33 @@
 import Head from "next/head";
-import About from "../components/About/About";
-import ContactMe from "../components/Contact/ContactMe";
-import Header from "../components/Header/Header";
-import Hero from "../components/Hero/Hero";
-import Projects from "../components/Projects/Projects";
-import Skills from "../components/Skills/Skills";
+
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+const DynamicHeader = dynamic(() => import("../components/Header/Header"), {
+  suspense: true,
+});
+
+const DynamicAbout = dynamic(() => import("../components/About/About"), {
+  suspense: true,
+});
+
+const DynamicContactMe = dynamic(
+  () => import("../components/Contact/ContactMe"),
+  { suspense: true }
+);
+
+const DynamicHero = dynamic(() => import("../components/Hero/Hero"), {
+  suspense: true,
+});
+
+const DynamicProjects = dynamic(
+  () => import("../components/Projects/Projects"),
+  { suspense: true }
+);
+
+const DynamicSkills = dynamic(() => import("../components/Skills/Skills"), {
+  suspense: true,
+});
 
 const Home = () => {
   return (
@@ -23,12 +46,14 @@ const Home = () => {
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <ContactMe />
+      <Suspense fallback={`Loading...`}>
+        <DynamicHeader />
+        <DynamicHero />
+        <DynamicAbout />
+        <DynamicSkills />
+        <DynamicProjects />
+        <DynamicContactMe />
+      </Suspense>
     </div>
   );
 };
